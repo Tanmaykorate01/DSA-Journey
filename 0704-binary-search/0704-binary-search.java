18aea9bee7032db9b7ca521ee377f4014ab2fb51
tanmay_korate <koratetanmay@gmail.com>
2026-07-07 00:22:26 +0530

    Time: 0 ms (100%), Space: 48.3 MB (49.79%) - LeetHub

--- 0704-binary-search/0704-binary-search.java
+++ 0704-binary-search/0704-binary-search.java
@@ -2,7 +2,7 @@ class Solution
 {
     public int search(int[] nums, int target) 
     {
-        int n = nums.length;
+        
         int beg = 0;
         int end = nums.length -1;
         int pos =-1;
