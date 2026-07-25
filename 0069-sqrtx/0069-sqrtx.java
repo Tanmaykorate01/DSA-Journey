7ddc8fde34c3c1368b0d34f6f9721b585d5d7356
tanmay_korate <koratetanmay@gmail.com>
2026-07-25 23:29:33 +0530

    Time: 1 ms (97.44%), Space: 42.8 MB (23.54%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.java
+++ 0069-sqrtx/0069-sqrtx.java
@@ -1,9 +1,43 @@
 class Solution 
+ 
 {
     public int mySqrt(int x) 
     {
-      return (int) Math.sqrt(x);          
+        if (x == 0 || x == 1)
+        {
+            return x;
+        }
+
+        int s = 0;
+        int end = x;
+        int ans = -1;
+
+        while (s <= end)
+        {
+            int mid = s + (end - s) / 2;
+
+            if (mid == x / mid)
+            {
+                return mid;
+            }
+            else if (mid > x / mid)
+            {
+                end = mid - 1;
+            }
+
+            if (mid < x / mid)
+            {
+                ans = mid;
+                s = mid + 1;
+            }
+        }
+
+        return ans;
     }
+}
+
+             
+    
             
 
 
@@ -11,4 +45,3 @@ class Solution
 
      
     
-}
