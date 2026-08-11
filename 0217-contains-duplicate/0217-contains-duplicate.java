e81efaee0f861389954c34a254cff69f6f89739c
tanmay_korate <koratetanmay@gmail.com>
2026-08-11 11:25:46 +0530

    Time: 38 ms (5.03%), Space: 110.4 MB (8.3%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.java
+++ 0217-contains-duplicate/0217-contains-duplicate.java
@@ -1,24 +1,23 @@
-class Solution 
-{
-    public boolean containsDuplicate(int[] nums)
-     {
-         HashSet<Integer> set = new HashSet<>();
+class Solution {
+    public boolean containsDuplicate(int[] nums) 
+    {
+
+        HashMap<Integer,Integer> h1 = new HashMap();
 
 
-         for(int i=0;i<nums.length;i++)
-         {
-            if(set.contains(nums[i]))
+        for(int n: nums)
+        {
+            h1.put(n,h1.getOrDefault(n,0)+1);
+        }
+
+        for(int i = 0;i<nums.length;i++)
+        {
+            if(h1.get(nums[i]) >  1)
             {
                 return true;
             }
-            
-                else
-                {
-                    set.add(nums[i]);
-                }
-            
-         }
+        }
         return false;
-        
     }
+
 }
