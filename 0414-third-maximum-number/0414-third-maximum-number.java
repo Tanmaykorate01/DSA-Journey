61a16b8684c92afcc380c7720bdd1e65dae91457
tanmay_korate <koratetanmay@gmail.com>
2026-08-17 09:30:59 +0530

    Time: 1 ms (98.93%), Space: 44.3 MB (98.76%) - LeetHub

--- 0414-third-maximum-number/0414-third-maximum-number.java
+++ 0414-third-maximum-number/0414-third-maximum-number.java
@@ -1,49 +1,46 @@
-class Solution
- {
+class Solution {
     public int thirdMax(int[] nums) 
     {
-        long first  = Long.MIN_VALUE;
-        long second = Long.MIN_VALUE;
-        long third = Long.MIN_VALUE;
+        long first =  Long.MIN_VALUE;
+          long second = Long.MIN_VALUE;
+            long third =  Long.MIN_VALUE;
 
 
-        for (int i =0 ;i<nums.length;i++)
-        {
-
-
-            if (nums[i] == first || nums[i] == second || nums[i] == third)
+            for(int i= 0; i<nums.length;i++)
             {
-                continue;
+
+                if(nums[i] == first||nums[i] == second||nums[i] == third)
+                {
+                    continue;
+                }
+                    if(nums[i]>first)
+                    {
+
+                        third = second;
+                        second = first;
+                        first = nums[i];
+                    }
+                    else  if(nums[i]>second)
+                    {
+                        third = second ;
+                        second = nums[i];
+                    }
+                    else
+                    {
+                        if(nums[i]>third)
+                        {
+                            third = nums[i];
+                        }
+                    }
             }
             
-       
-            if (nums[i]>first)
+
+            if(third == Long.MIN_VALUE)
             {
-
-                third = second;
-                second = first;
-                first = nums[i];
-
-
+                return (int) first;
             }
-            else if(nums[i]>second)
-            {
-                third = second;
-                second = nums[i];
-            }
-            else
-            {
-                if (nums[i]>third)
-                {
-                    third = nums[i];
-                }
-            }
-
-        }
-        if (third == Long.MIN_VALUE)
-    return (int) first;
-
-return (int) third;
-    }
+            
+            return (int) third;
         
+    }
 }
