a5ca75da6720eacdb52bf8e9b520b9cbb02b5aae
tanmay_korate <koratetanmay@gmail.com>
2026-09-11 19:01:14 +0530

    Time: 16 ms (21.14%), Space: 46.7 MB (16.07%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -1,39 +1,31 @@
 class Solution {
-    public boolean isAnagram(String s, String t) 
-    {
-          if (s.length()!= t.length())
+    public boolean isAnagram(String s, String t)
+     {
+        HashMap <Character, Integer> h1 = new HashMap();
+
+        for(int i = 0;i<s.length();i++)
         {
-            return false;
-    
-            }
-        else
-        {
-            Map<Character,Integer> map1 = new HashMap<>();
-
-            for (int i = 0;i<s.length();i++)
-            {
-                char ch = s.charAt(i);
-                map1.put(ch, map1.getOrDefault(ch,0)+1);
-            }
-
-
-            for (int i  = 0;i<t.length();i++)
-            {
-                char ch = t.charAt(i);
-                map1.put(ch, map1.getOrDefault(ch,0)-1);
-            }
-
-            for (int val : map1.values())
-            {
-                if (val!=0)
-                {
-                    return false;
-                }
-
-            }
-return true;
+            char ch = s.charAt(i);
+            h1.put(ch,h1.getOrDefault(ch,0)+1);
         }
 
+        for(int i = 0;i<t.length();i++)
+        {
+            char ch = t.charAt(i);
+            h1.put(ch,h1.getOrDefault(ch,0)-1);
+        }
         
+
+        for(int val : h1.values())
+        {
+            if(val != 0)
+            {
+                return false;
+            }
+        
+        }
+
+        return true;
+
     }
 }
