24513b7230644bb8bd1997e39faf1a4969c33e2b
tanmay_korate <koratetanmay@gmail.com>
2026-09-15 09:57:11 +0530

    Time: 1 ms (59.91%), Space: 42.8 MB (96.1%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.java
+++ 0014-longest-common-prefix/0014-longest-common-prefix.java
@@ -1,31 +1,18 @@
 class Solution {
+    public String longestCommonPrefix(String[] strs) 
+    {
+        Arrays.sort(strs);
 
-    public String longestCommonPrefix(String[] strs) {
+        String first  = strs[0];
+        String last  =  strs[strs.length-1];
 
-        if (strs == null || strs.length == 0) {
-            return "";
+        int i =0 ;
+        while(i<first.length()&& i<last.length() && first.charAt(i) == last.charAt(i))
+        {
+            i++;
         }
 
-        String ans = "";
-
-        // Traverse each character of the first string
-        for (int j = 0; j < strs[0].length(); j++) {
-
-            char ch = strs[0].charAt(j);
-
-            // Compare this character with every other string
-            for (int i = 1; i < strs.length; i++) {
-
-                // If current string is shorter OR character doesn't match
-                if (j >= strs[i].length() || strs[i].charAt(j) != ch) {
-                    return ans;
-                }
-            }
-
-            // All strings have the same character at position j
-            ans += ch;
-        }
-
-        return ans;
+        return first.substring(0,i);
+        
     }
 }
